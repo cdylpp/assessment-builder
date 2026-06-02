62f0f088996825da6b803f019a23e5e4495b1787
cdylpp <cdylpp@yahoo.com>
2026-06-02 15:14:49 -0700

    added db

--- api/main.py
+++ api/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.routes import router
+from app.db import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Assessment Builder API")
 app.include_router(router)
