a749645a631b1f300a28d3a45d29ae78b9a799bb
cdylpp <cdylpp@yahoo.com>
2026-06-02 14:33:57 -0700

    added db bootstrap

--- api/app/db.py
+++ api/app/db.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+# Bootstrap the database
 DATABASE_URL = os.environ["DATABASE_URL"]
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
